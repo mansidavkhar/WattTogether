e4a206a8c5f0f71c6c9f74dfda6d0ad485f31449
ms_davkhar <davkharmansi29@gmail.com>
2025-09-26 03:18:19 +0530

    clean files

--- frontend/src/components/navbar/MemberNavbar.jsx
+++ frontend/src/components/navbar/MemberNavbar.jsx
@@ -1,37 +1,3 @@
-// import logo from '../../assets/wattTogether_logo.png';
-// import {Link} from 'react-router-dom';
-
-// const MemberNavbar = () => {
-
-//     return (
-//       <div className='navbar flex items-center justify-between py-3 px-10 lg:flex-row bg-[#508C9B]'>
-//           <div>
-//             <a href="/member/home" className="text-white font-[inter] text-3xl tracking-wider flex items-center"><img src={logo} alt="platform logo" /></a>
-//           </div>
-
-//           <div >
-//             {/*for responsiveness add classes in the above div */}
-//             <div className="space-x-10">
-//               {/* <Link to="/member/home" className="text-white text-xl hover:underline underline-offset-8 px-5 py-2">Home</Link> */}
-//               <Link to="/member/browsecampaigns" className="text-white text-xl hover:underline underline-offset-8 px-5 py-2">Browse Campaigns</Link>
-//               <Link to="/member/myinvestments" className="text-white text-xl hover:underline underline-offset-8 px-5 py-2">My Investments</Link>
-//               <Link to="/member/projects" className="text-white text-xl hover:underline underline-offset-8 px-5 py-2">My Projects</Link>
-//               <Link to="/member/network" className="text-white text-xl hover:underline underline-offset-8 px-5 py-2">Network</Link>
-//               <Link to="/member/wallet" className="text-white text-xl hover:underline underline-offset-8 px-5 py-2">Wallet</Link>
-//               <button className="text-white text-xl hover:underline underline-offset-8 px-5 py-2 font-bold">Logout</button>
-//              </div>
-//           </div>
-//       </div>
-//     )
-//   }
-
-//   export default MemberNavbar;
-
-
-
-
-
-import React from 'react';
 import logo from '../../assets/wattTogether_logo.png';
 import { Link, useNavigate } from 'react-router-dom';
 
